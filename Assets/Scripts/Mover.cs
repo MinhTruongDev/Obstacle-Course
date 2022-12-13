44a30ab03ec939e43a1d77e920aaf2173489cb2f
Phan Minh Trường <104303855+MinhTruongDev@users.noreply.github.com>
2022-12-13 12:26:06 +0700

    add follow camera

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField]
+    float yValue = 0.01f;
+
+    [SerializeField]
+    float moveSpeed = 15f;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        transform.Translate (xValue, yValue, zValue);
+    }
+}
